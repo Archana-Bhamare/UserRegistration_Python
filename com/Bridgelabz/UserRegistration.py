55ad46a6024df6b3978e351815537d5682e12187
Archana-Bhamare <archusonar2@gmail.com>
2020-11-30 18:13:10 +0530

    Added : User Last Name

--- com/Bridgelabz/UserRegistration.py
+++ com/Bridgelabz/UserRegistration.py
@@ -1,1 +1,29 @@
-print("Welcome to User Registration...")
+import re
+
+
+class UserRegistration:
+    pattern = '^[A-Z]{1}[a-z]{3,}$'
+
+    # User First Name
+    def user_firstname(self):
+        user_name = input("Enter the User First Name : ")
+        result = re.match(UserRegistration.pattern, user_name)
+        if result:
+            print("Valid Name")
+        else:
+            print("Invalid Name, Please enter name in proper format")
+
+    # User Last Name
+    def user_lastname(self):
+        user_name = input("Enter the User Last Name : ")
+        result = re.match(UserRegistration.pattern, user_name)
+        if result:
+            print("Valid Name")
+        else:
+            print("Invalid Name, Please enter Name in proper format")
+
+
+if __name__ == "__main__":
+    user = UserRegistration()
+    user.user_firstname()
+    user.user_lastname()
