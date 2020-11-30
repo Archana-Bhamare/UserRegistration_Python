88c49bb6e8228da62df302a810ae4db9e9147f2a
Archana-Bhamare <archusonar2@gmail.com>
2020-11-30 21:23:46 +0530

    Refactor : Refactor the code

--- com/Bridgelabz/UserRegistration.py
+++ com/Bridgelabz/UserRegistration.py
@@ -9,48 +9,58 @@ class UserRegistration:
 
     # User First Name
     def user_firstname(self):
-        user_name = input("Enter the User First Name : ")
-        result = re.match(UserRegistration.NAME_REGEX, user_name)
-        if result:
-            print("Valid Name")
-        else:
-            print("Invalid Name, Please enter name in proper format")
+        while True:
+            user_name = input("Enter the User First Name : ")
+            result = re.match(UserRegistration.NAME_REGEX, user_name)
+            if result:
+                print("Valid Name")
+                break
+            else:
+                print("Invalid Name, Please enter name in proper format")
 
     # User Last Name
     def user_lastname(self):
-        user_name = input("Enter the User Last Name : ")
-        result = re.match(UserRegistration.NAME_REGEX, user_name)
-        if result:
-            print("Valid Name")
-        else:
-            print("Invalid Name, Please enter Name in proper format")
+        while True:
+            user_name = input("Enter the User Last Name : ")
+            result = re.match(UserRegistration.NAME_REGEX, user_name)
+            if result:
+                print("Valid Name")
+                break
+            else:
+                print("Invalid Name, Please enter Name in proper format")
 
     # User Email
     def user_email(self):
-        user_email = input("Enter the Email Id : ")
-        result = re.match(UserRegistration.EMAIL_REGEX, user_email)
-        if result:
-            print("Valid Email Id")
-        else:
-            print("Invalid Email Id")
+        while True:
+            user_email = input("Enter the Email Id : ")
+            result = re.match(UserRegistration.EMAIL_REGEX, user_email)
+            if result:
+                print("Valid Email Id")
+                break
+            else:
+                print("Invalid Email Id")
 
     # User Email
     def user_mobnumber(self):
-        user_mobnumber = input("Enter the Mobile Number : ")
-        result = re.match(UserRegistration.MOBILE_REGEX, user_mobnumber)
-        if result:
-            print("Valid Mobile Number")
-        else:
-            print("Invalid Mobile Number")
+        while True:
+            user_mobnumber = input("Enter the Mobile Number : ")
+            result = re.match(UserRegistration.MOBILE_REGEX, user_mobnumber)
+            if result:
+                print("Valid Mobile Number")
+                break
+            else:
+                print("Invalid Mobile Number")
 
     # User Password
     def user_password(self):
-        user_password = input("Enter the Password : ")
-        result = re.match(UserRegistration.PASSWD_REGEX, user_password)
-        if result:
-            print("Valid Password")
-        else:
-            print("Invalid Password, Please enter valid password")
+        while True:
+            user_password = input("Enter the Password : ")
+            result = re.match(UserRegistration.PASSWD_REGEX, user_password)
+            if result:
+                print("Valid Password")
+                break
+            else:
+                print("Invalid Password, Please enter valid password")
 
 
 if __name__ == "__main__":
