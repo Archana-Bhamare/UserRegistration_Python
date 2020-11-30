bb13271b1e17820b9c8e773c1ecd9787ed43a702
Archana-Bhamare <archusonar2@gmail.com>
2020-11-30 21:16:34 +0530

    Refactor : Refactor the code

--- com/Bridgelabz/UserRegistration.py
+++ com/Bridgelabz/UserRegistration.py
@@ -7,30 +7,36 @@ class UserRegistration:
 
     # User First Name
     def user_firstname(self):
-        user_name = input("Enter the User First Name : ")
-        result = re.match(UserRegistration.NAME_REGEX, user_name)
-        if result:
-            print("Valid Name")
-        else:
-            print("Invalid Name, Please enter name in proper format")
+        while True:
+            user_name = input("Enter the User First Name : ")
+            result = re.match(UserRegistration.NAME_REGEX, user_name)
+            if result:
+                print("Valid Name")
+                break
+            else:
+                print("Invalid Name, Please enter name in proper format")
 
     # User Last Name
     def user_lastname(self):
-        user_name = input("Enter the User Last Name : ")
-        result = re.match(UserRegistration.NAME_REGEX, user_name)
-        if result:
-            print("Valid Name")
-        else:
-            print("Invalid Name, Please enter Name in proper format")
+        while True:
+            user_name = input("Enter the User Last Name : ")
+            result = re.match(UserRegistration.NAME_REGEX, user_name)
+            if result:
+                print("Valid Name")
+                break
+            else:
+                print("Invalid Name, Please enter Name in proper format")
 
     # User Email
     def user_email(self):
-        user_email = input("Enter the Email Id : ")
-        result = re.match(UserRegistration.EMAIL_REGEX, user_email)
-        if result:
-            print("Valid Email Id")
-        else:
-            print("Invalid Emaild Id")
+        while True:
+            user_email = input("Enter the Email Id : ")
+            result = re.match(UserRegistration.EMAIL_REGEX, user_email)
+            if result:
+                print("Valid Email Id")
+                break
+            else:
+                print("Invalid Emaild Id")
 
 
 if __name__ == "__main__":
