a644c99cadeef4b470182f8eb0f67b3446d68e9a
Archana-Bhamare <archusonar2@gmail.com>
2020-11-30 18:33:57 +0530

    Added : User Email Id

--- com/Bridgelabz/UserRegistration.py
+++ com/Bridgelabz/UserRegistration.py
@@ -1,1 +1,40 @@
-print("Welcome to User Registration...")
+import re
+
+
+class UserRegistration:
+    NAME_REGEX = '^[A-Z]{1}[a-z]{3,}$'
+    EMAIL_REGEX = '^[a-zA-Z0-9]+([.+-_][a-z-A-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$'
+
+    # User First Name
+    def user_firstname(self):
+        user_name = input("Enter the User First Name : ")
+        result = re.match(UserRegistration.NAME_REGEX, user_name)
+        if result:
+            print("Valid Name")
+        else:
+            print("Invalid Name, Please enter name in proper format")
+
+    # User Last Name
+    def user_lastname(self):
+        user_name = input("Enter the User Last Name : ")
+        result = re.match(UserRegistration.NAME_REGEX, user_name)
+        if result:
+            print("Valid Name")
+        else:
+            print("Invalid Name, Please enter Name in proper format")
+
+    # User Email
+    def user_email(self):
+        user_email = input("Enter the Email Id : ")
+        result = re.match(UserRegistration.EMAIL_REGEX, user_email)
+        if result:
+            print("Valid Email Id")
+        else:
+            print("Invalid Emaild Id")
+
+
+if __name__ == "__main__":
+    user = UserRegistration()
+    user.user_firstname()
+    user.user_lastname()
+    user.user_email()
