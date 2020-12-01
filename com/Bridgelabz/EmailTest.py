38962ab712460af2d1665e554e8b847faa2d0fb3
Archana-Bhamare <archusonar2@gmail.com>
2020-12-01 11:43:36 +0530

    Some changes occured

--- com/Bridgelabz/EmailTest.py
+++ com/Bridgelabz/EmailTest.py
@@ -2,9 +2,9 @@ import re
 
 
 class EmailTest:
-    REGEX = '^[a-zA-Z]+([.+-_][a-z-A-Z0-9]+)*@{1}[a-zA-Z]+.[a-z]{2,3}([.][a-z]{2})?$'
+    REGEX = '^[a-zA-Z0-9]+([.+-_][a-z-A-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2,3})?$'
     # Valid Email Ids
-    valid_email = {
+    valid_email = [
         "abc@yahoo.com",
         "abc-100@yahoo.com",
         "abc.100@yahoo.com",
@@ -14,9 +14,9 @@ class EmailTest:
         "abc@1.com",
         "abc@gmail.com.com",
         "abc+100@gmail.com",
-    }
+    ]
     # Invalid Email Ids
-    invalid_email = {
+    invalid_email = [
         "abc@.com.my",
         "abc123@gmail.a",
         "abc123@.com",
@@ -28,13 +28,13 @@ class EmailTest:
         "abc@abc@gmail.com",
         "abc@gmail.com.1a",
         "abc@gmail.com.aa.au"
-    }
+    ]
 
     # EMail Validation
     def email_validate(self, request):
         """
 
-        :param request: i(Dictionary)-valid_email and invalid_email
+        :param request: i-Taking the emails from array (valid_email and invalid_email)
         :return: match email ids with respective regex pattern
         """
         return re.fullmatch(EmailTest.REGEX, request)
